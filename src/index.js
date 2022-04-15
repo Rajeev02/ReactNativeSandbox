30399ba4b7d1f9593ce141c4cb52c944f9dcb2cd
Rajeev Joshi <rajeevjoshi91@gmail.com>
2022-04-15 17:43:41 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+let greeting;
+const date = new Date();
+const currentTime = date.getHours();
+
+var customStyle = { color: "red" };
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Evening";
+  customStyle.color = "blue";
+}
+
+ReactDom.render(
+  <div>
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
